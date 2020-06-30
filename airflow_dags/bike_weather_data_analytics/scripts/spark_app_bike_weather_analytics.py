2689ef69a5ba8eb33937de626b882bd9d42c94cb
jwd <jwd.devlin@gmail.com>
2020-06-30 22:25:30 +0200

    bike_weather data

---
+++ airflow_dags/bike_weather_data_analytics/scripts/spark_app_bike_weather_analytics.py
@@ -0,0 +1,34 @@
+import pyspark
+import sys
+from pyspark.sql import functions as F
+
+class BikeWeatherAnalyticsCreate:
+    def __init__(self, database: str):
+        self.database = database
+        self.monogdb_ip = "host.docker.internal"
+        self.monogdb_details = "mongodb://{ip}/{db}".format(ip=self.monogdb_ip, db=self.database)
+        self.spark = pyspark.sql.SparkSession.builder \
+            .appName("bike_weather_collection") \
+            .config("spark.mongodb.input.uri", self.monogdb_details) \
+            .config("spark.mongodb.output.uri", self.monogdb_details) \
+           .getOrCreate()
+        self.bike_weather_analytics_collection = "average_daily_trip_duration_plus_count"
+
+    def __average_daily_trip_duration_plus_count(self, df_bike_weather_collection: pyspark.sql.DataFrame,
+                                                 bike_weather_analytics_collection:str):
+        average_daily_bike_clean_data = df_bike_weather_collection.select("weather_date",
+                                                                          "tripduration").groupBy("weather_date").agg(
+            F.mean('tripduration'), F.count("starttime"))
+        average_daily_bike_clean_data.write.format("mongo").option("collection",
+                                                            bike_weather_analytics_collection).mode("append").save()
+
+    def execute(self, bike_weather_collection: str, bike_weather_analytics_collection: str):
+        df_bike_weather_collection = self.spark.read.format("mongo").option("collection",
+                                                                            bike_weather_collection).load()
+
+        self.__average_daily_trip_duration_plus_count(df_bike_weather_collection, bike_weather_analytics_collection)
+
+
+if __name__ == '__main__':
+    BikeWeatherAnalyticsCreate(database="local").execute(df_bike_weather_collection=sys.argv[1],
+                                                         bike_weather_analytics_collection = sys.argv[2])
---
+++ airflow_dags/bike_weather_data_analytics/scripts/spark_app_bike_weather_collection.py
@@ -0,0 +1,29 @@
+import pyspark
+import sys
+
+class BikeWeatherCollectionCreate:
+    def __init__(self, database: str):
+        self.database = database
+        self.monogdb_ip = "host.docker.internal"
+        self.monogdb_details = "mongodb://{ip}/{db}".format(ip=self.monogdb_ip, db=self.database)
+        self.spark = pyspark.sql.SparkSession.builder \
+            .appName("bike_weather_collection") \
+            .config("spark.mongodb.input.uri", self.monogdb_details) \
+            .config("spark.mongodb.output.uri", self.monogdb_details) \
+           .getOrCreate()
+
+    def execute(self, bike_collection: str, weather_collection:str , bike_weather_collection:str):
+        df_bike_collection = self.spark.read.format("mongo").option("collection", bike_collection).load()
+        bike_count = df_bike_collection.count()
+        df_weather_collection =  self.spark.read.format("mongo").option("collection", weather_collection).load()
+        weather_count = df_weather_collection.count()
+        print(" Weather df size ",weather_count , " , bike df ", bike_count)
+        #join
+        df_bike_weather = df_bike_collection.join(df_weather_collection, df_bike_collection.weather_date == df_weather_collection.date, "inner")
+        bike_weather_count = df_bike_weather.count()
+        print(" Weather and bike count ", bike_weather_count)
+        df_bike_weather.write.format("mongo").option("collection", bike_weather_collection).mode("append").save()
+
+
+if __name__ == '__main__':
+    BikeWeatherCollectionCreate(database="local").execute(bike_collection=sys.argv[1], weather_collection=sys.argv[2], bike_weather_collection = sys.argv[3])
